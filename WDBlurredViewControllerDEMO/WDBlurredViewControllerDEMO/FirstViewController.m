ef549e1480082b808255ea2a2cc50b17f1f8c649
KimDarren <korean.darren@gmail.com>
2014-12-07 16:26:23 +0900

    Add comments, Add example to set the background style.

--- WDBlurredViewControllerDEMO/WDBlurredViewControllerDEMO/FirstViewController.m
+++ WDBlurredViewControllerDEMO/WDBlurredViewControllerDEMO/FirstViewController.m
@@ -10,8 +10,15 @@
 
 - (IBAction)showBlurredModal:(id)sender
 {
+    // Load view controller from storyboard.
     SecondViewController *vc = [self.storyboard instantiateViewControllerWithIdentifier:SecondViewControllerId];
-    vc.blurStyle = UIBlurEffectStyleDark;
+    // Set the blur effect style.
+    vc.blurStyle = UIBlurEffectStyleLight;
+    // Set the color of blur view.
+    vc.backgroundColor = [UIColor yellowColor];
+    // Set the alpha value of background color of blur view. (Default : 0.5f)
+    vc.backgroundAlpha = 0.3f;
+    // Present the view controller.
     [self presentViewController:vc animated:NO completion:nil];
 }
 
